6aaf6908f963460685050afe3190fd4c19f08914
Luston123 <nzuzimabiala29@gmail.com>
2026-02-04 11:51:18 +0000

    Set up Express server to serve front-end files

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,14 @@
 
+const express = require('express');
+const app = express();
+const PORT = 3000;
+
+app.use(express.static('../')); // Serve arquivos do front-end
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/../index.html');
+});
+
+app.listen(PORT, () => {
+    console.log(`Servidor rodando em http://localhost:${PORT}`);
+});
